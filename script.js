d2699e9a1fc77c7e6a36539afc68897fe5267576
Umair Akram <164014544+imumairakram@users.noreply.github.com>
2024-05-11 10:59:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function toggleMenu() {
 
 
   // List of words to type
-  const words = ["Website Developer", "WordPress Developer", "Video Editor"];
+  const words = ["Website Developer", "WordPress Developer", "Video Editor", "Coder"];
   let currentWordIndex = 0;
   let currentCharIndex = 0;
   let typingSpeed = 70; 
@@ -49,41 +49,37 @@ function toggleMenu() {
 
 
   
-  // Function to show the message when the "Live Demo" button is clicked
+  // Function to show the 1st message when the "Live Demo" button is clicked
   function showDemoMessage1() {
     var message = document.getElementById("demoMessage");
     message.style.display = "block";
-    // Hide the message after 4 seconds
     setTimeout(function () {
       message.style.display = "none";
     }, 3000);
   }
 
-  //Function to show the message when the "Live Demo" button is clicked
+  //Function to show the 2nd message when the "Live Demo" button is clicked
   function showDemoMessage2() {
     var message = document.getElementById("demoMessage");
     message.style.display = "block";
-    // Hide the message after 4 seconds
     setTimeout(function () {
       message.style.display = "none";
     }, 3000);
   }
 
-  // Function to show the message when the "Live Demo" button is clicked
+  // Function to show the 3rd message when the "Live Demo" button is clicked
   function showDemoMessage3() {
     var message = document.getElementById("demoMessage");
     message.style.display = "block";
-    // Hide the message after 4 seconds
     setTimeout(function () {
       message.style.display = "none";
     }, 3000);
   }
 
-  // Function to show the message when the "Live Demo" button is clicked
+  // Function to show the 4th message when the "Live Demo" button is clicked
   function showDemoMessage4() {
     var message = document.getElementById("demoMessage");
     message.style.display = "block";
-    // Hide the message after 4 seconds
     setTimeout(function () {
       message.style.display = "none";
     }, 3000);
